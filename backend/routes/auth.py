8a6c704637aeb8328c4368ae9dc2821166b48a3a
Rolando Rios <rolando_rios93@hotmail.com>
2026-01-13 20:36:32 -0600

    se agrega ruta temporal para creacion de nuevo usuari atravez de un token, funcion register

--- backend/routes/auth.py
+++ backend/routes/auth.py
@@ -27,6 +27,27 @@ def login_post():
     
     return redirect(url_for('auth_bp.login'),logit=logit)
 
+
+@auth_bp.route('/register/<token>', methods =['GET','POST'])
+def register(token):
+    #valodar el otken (podria ser un codigo que se guarde en la DB con tiempo de expiracion)
+    if not auth_service.calidar_token_registro(token):
+        flash("El enlace de registro es invalido o ha expirado","danger")
+        return redirect(url_for('auth_bp.login'))
+    
+    if request.method == 'POST':
+
+        #metodo para crear el primer usuario admin
+        email = request.form.get('email')
+        password = request.form.get('password')
+        
+        exito =auth_service.crear_usuario_inicial(email,password,token)
+        if exito:
+            flash("Cuenta configurada con exito . Ta puedes iniciar sesion","success")
+            return redirect(url_for('auth_bp.login'))
+    return render_template('register.html', token=token)
+
+
 @auth_bp.route('/logout')
 @login_required
 def logout():
